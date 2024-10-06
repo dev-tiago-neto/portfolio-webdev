5582682a96b948126697891ebce59bb3cd90d9fd
Tiago Morais <tiagomoraisneto@gmail.com>
2024-10-06 16:47:00 -0300

    Página de projetos

---
+++ src/pages/Projetos.jsx
@@ -0,0 +1,9 @@
+import { ProjectsList } from "../components/Projects/List";
+
+export default function Projetos() {
+  return (
+    <div className="container mx-auto">
+      <ProjectsList />
+    </div>
+  );
+}
