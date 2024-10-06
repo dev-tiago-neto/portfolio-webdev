3b2f3bc5e5efa2bd542f97af453111bbe909efe1
Tiago Morais <tiagomoraisneto@gmail.com>
2024-10-06 17:50:07 -0300

    Adicionar página de contato

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,6 @@ export const Footer = () => {
           <div className="text-lg text-ternary-dark dark:text-ternary-light">
             &copy; {new Date().getFullYear()}
             <a
-              href="https://github.com/realstoman/react-tailwindcss-portfolio"
               target="__blank"
               className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
             >
@@ -50,7 +49,7 @@ export const Footer = () => {
             </a>
             .
             <span
-              href="#"
+              href="https://github.com/dev-tiago-neto"
               target="__blank"
               className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
             >
